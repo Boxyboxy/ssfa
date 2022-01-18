08aa99f34010a3095141c2bc62b0f0d4a614c6ad
boxyboxy <eugenengboxiang@gmail.com>
2022-01-18 09:30:55 +0800

    initial push

---
+++ src/test/java/ibf/ssf/ssfa/SsfaApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf.ssf.ssfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
